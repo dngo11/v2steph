7c83cf11654cc132f8030394d83393b05c472b88
David <dngo11@stanford.edu>
2014-01-03 16:24:16 -0500

    only password

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
-$username = "steph"; //Change to whatever you want your username to be
 $password = "david"; //Change to whatever you want your password to be
 
 if(isset($_POST['submit'])){
-	if($_POST['username'] == $username && $_POST['password'] == $password){
+	if($_POST['password'] == $password){
         include_once("home.html");
         echo "Welcome to Steph Habif's portfolio.";
         } else {
-        echo "sorry the username and password were incorrect";
+        echo "sorry the password were incorrect";
         }
 } else { //IF THE FORM WAS NOT SUBMITTED
 //SHOW FORM
 	?><form method="post">
-		Username: <input type="text" name="username" /><br />
 		Password: <input type="password" name="password" />
 		<input type='submit' name='submit' />
 	</form><?php
