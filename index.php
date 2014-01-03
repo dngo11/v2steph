16326b1c075ce21b39b9b4dedadd50d2151effaf
David <dngo11@stanford.edu>
2014-01-03 16:19:54 -0500

    added pw protection

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php include_once("home.html"); ?>
+<?php
+include_once("home.html");
+$username = "steph"; //Change to whatever you want your username to be
+$password = "david"; //Change to whatever you want your password to be
+
+if(isset($_POST['submit'])){
+	if($_POST['username'] == $username && $_POST['password'] == $password){
+        //EXECUTE YOUR CODE HERE
+        echo "Welcome to Steph Habif's portfolio.";
+        } else {
+        echo "sorry the username and password were incorrect";
+        }
+} else { //IF THE FORM WAS NOT SUBMITTED
+//SHOW FORM
+	?><form method="post">
+		Username: <input type="text" name="username" /><br />
+		Password: <input type="password" name="password" />
+		<input type='submit' name='submit' />
+	</form><?php
+}
+
+
+?>
