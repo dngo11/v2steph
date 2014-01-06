3ae057127ce5ad598c133dc79f7615a5a31e89e5
David <dngo11@stanford.edu>
2014-01-05 23:35:43 -0500

    rounded password ox

--- index.php
+++ index.php
@@ -112,11 +112,11 @@ if(isset($_POST['submit'])){
 		<div class="span12">
 			<div class="row-fluid">
         		<section class="container">
-        			<article class="span12">
+        			<article class="center span6">
                         </br>
         				<h1 class="center FertigoPro-Italic"><span>Access Habif Portfolio</span></h1></br>
 						<form class="center" method="post">
-						<input class="center FertigoPro-Italic" type="password" name="password" placeholder="type in password here" />
+						<input class="rounded center FertigoPro-Italic" type="password" name="password" placeholder="type in password here" />
 						<input id="passwordsubmit" class="center blue FertigoPro-Italic" type='submit' name='submit' />
 						</form><?php	
 	}
